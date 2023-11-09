b8d885b8fcabec826d90b12715620dc05d17c540
sharath6402 <106332843+sharath6402@users.noreply.github.com>
2023-11-09 20:33:09 +0530

    Add files via upload

---
+++ singleplayer.py
@@ -0,0 +1,496 @@
+from tkinter import *
+
+win = Tk()
+win.title("TIC TAC TOE 1 PLAYERS")
+win.geometry("350x350")
+win.configure(background = "blue")
+turn = 1
+sign = 'O'
+poswin = 1
+board=['','','','','','','','','']
+
+def windecide():
+    global board
+    global turn
+    if board[0]==board[1]==board[2]=='O' or board[0]==board[1]==board[2]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+    if board[3]==board[5]==board[4]=='O' or board[4]==board[5]==board[3]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+    if board[7]==board[8]==board[6]=='O' or board[6]==board[7]==board[8]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+    if board[0]==board[4]==board[8]=='O' or board[0]==board[4]==board[8]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+    if board[0]==board[3]==board[6]=='O' or board[0]==board[3]==board[6]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+    if board[1]==board[4]==board[7]=='O' or board[4]==board[1]==board[7]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+    if board[2]==board[5]==board[8]=='O' or board[2]==board[5]==board[8]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+    if board[6]==board[4]==board[2]=='O' or board[6]==board[4]==board[2]=='X':
+        winner.configure(text=f"Winner player {turn}")
+        turn = 3
+
+
+def nextpos():
+    global board
+    global new_pos
+    global poswin 
+    count = 0
+   #empty_positions = [index for index, char in enumerate(board) if char == '']
+    xpos = [index for index, char in enumerate(board) if char == 'X']
+    opos = [index for index, char in enumerate(board) if char == 'O']
+    print(xpos)
+    lenxpos = len(xpos)
+    if poswin:
+        if lenxpos == 0:
+            if 0 in opos :
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 2 in opos :
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 6 in opos :
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 8 in opos :
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif board[4]=="":
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            else:
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+                
+                
+        if lenxpos == 1:
+            
+            
+            if 2 in opos and 8 in opos and board[5]=='':
+                
+                new_pos=5
+                xpos.append(5)
+                secplaygame()
+            elif 6 in opos and 8 in opos and board[7]=='':
+                
+                new_pos=7
+                xpos.append(7)
+                secplaygame()
+            elif 6 in opos and 7 in opos and board[8]=='':
+                
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 5 in opos and 8 in opos and board[2]=='':
+                
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 7 in opos and 8 in opos and board[6]=='':
+                
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            
+            elif 0 in opos and 3 in opos and board[6]=='':
+                
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 3 in opos and 6 in opos and board[0]=='':
+                
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif 0 in opos and 1 in opos and board[2]=='':
+                
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 0 in opos and 2 in opos and board[1]=='':
+                
+                new_pos=1
+                xpos.append(1)
+                secplaygame()
+
+            elif 2 in opos and 5 in opos and board[8]=='':
+                
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+
+            elif 0 in opos and 7 in opos and board[2]=='':
+                
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 0 in opos and 4 in opos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 2 in opos and 4 in opos and board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            #elif 0 in opos and 2 in opos and board[1]=='':
+             #   new_pos=1
+              #  xpos.append(1)
+               # secplaygame()
+            elif 0 in opos and 6 in opos and board[3]=='':
+                new_pos=3
+                xpos.append(3)
+                secplaygame()
+            elif 2 in opos and 4 in opos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif xpos[0]==0 | xpos[0]==4:
+                if board[1]=="":
+                    new_pos=1
+                    xpos.append(1)
+                    secplaygame()
+                elif board[3]=="":
+                    new_pos=3
+                    xpos.append(3)
+                    secplaygame()
+            elif board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+
+        if lenxpos >= 2:
+            if 0 in xpos and 1 in xpos and board[2]=='':
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 0 in xpos and 2 in xpos and board[1]=='':
+                new_pos=1
+                xpos.append(1)
+                secplaygame()
+            elif 2 in xpos and 1 in xpos and board[0]=='':
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif 3 in xpos and 4 in xpos and board[5]=='':
+                new_pos=5
+                xpos.append(5)
+                secplaygame()
+            elif 3 in xpos and 5 in xpos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 4 in xpos and 5 in xpos and board[3]=='':
+                new_pos=3
+                xpos.append(3)
+                secplaygame()
+            elif 6 in xpos and 7 in xpos and board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 8 in xpos and 6 in xpos and board[7]=='':
+                new_pos=7
+                xpos.append(7)
+                secplaygame()
+            elif 7 in xpos and 8 in xpos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 0 in xpos and 3 in xpos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 0 in xpos and 6 in xpos and board[3]=='':
+                new_pos=3
+                xpos.append(3)
+                secplaygame()
+            elif 3 in xpos and 6 in xpos and board[0]=='':
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif 7 in xpos and 1 in xpos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 4 in xpos and 7 in xpos and board[1]=='':
+                new_pos=1
+                xpos.append(1)
+                secplaygame()
+            elif 7 in xpos and 1 in xpos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 5 in xpos and 8 in xpos and board[2]=='':
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 2 in xpos and 8 in xpos and board[5]=='':
+                new_pos=5
+                xpos.append(5)
+                secplaygame()
+            elif 2 in xpos and 5 in xpos and board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 0 in xpos and 4 in xpos and board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 0 in xpos and 8 in xpos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 4 in xpos and 8 in xpos and board[0]=='':
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif 4 in xpos and 6 in xpos and board[2]=='':
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 2 in xpos and 4 in xpos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 2 in xpos and 6 in xpos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+
+            if 0 in opos and 1 in opos and board[2]=='':
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 0 in opos and 2 in opos and board[1]=='':
+                new_pos=1
+                xpos.append(1)
+                secplaygame()
+            elif 2 in opos and 1 in opos and board[0]=='':
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif 3 in opos and 4 in opos and board[5]=='':
+                new_pos=5
+                xpos.append(5)
+                secplaygame()
+            elif 3 in opos and 5 in opos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 4 in opos and 5 in opos and board[3]=='':
+                new_pos=3
+                xpos.append(3)
+                secplaygame()
+            elif 6 in opos and 7 in opos and board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 8 in opos and 6 in opos and board[7]=='':
+                new_pos=7
+                xpos.append(7)
+                secplaygame()
+            elif 7 in opos and 8 in opos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 0 in opos and 3 in opos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 0 in opos and 6 in opos and board[3]=='':
+                new_pos=3
+                xpos.append(3)
+                secplaygame()
+            elif 3 in opos and 6 in opos and board[0]=='':
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif 7 in opos and 1 in opos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 4 in opos and 7 in opos and board[1]=='':
+                new_pos=1
+                xpos.append(1)
+                secplaygame()
+            elif 7 in opos and 1 in opos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 5 in opos and 8 in opos and board[2]=='':
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 2 in opos and 8 in opos and board[5]=='':
+                new_pos=5
+                xpos.append(5)
+                secplaygame()
+            elif 2 in opos and 5 in opos and board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 0 in opos and 4 in opos and board[8]=='':
+                new_pos=8
+                xpos.append(8)
+                secplaygame()
+            elif 0 in opos and 8 in opos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            elif 4 in opos and 8 in opos and board[0]=='':
+                new_pos=0
+                xpos.append(0)
+                secplaygame()
+            elif 4 in opos and 6 in opos and board[2]=='':
+                new_pos=2
+                xpos.append(2)
+                secplaygame()
+            elif 2 in opos and 4 in opos and board[6]=='':
+                new_pos=6
+                xpos.append(6)
+                secplaygame()
+            elif 2 in opos and 6 in opos and board[4]=='':
+                new_pos=4
+                xpos.append(4)
+                secplaygame()
+            else:
+                new_pos = [index for index, char in enumerate(board) if char == '' ]
+                new_pos = new_pos[0]
+                new_pos=int(new_pos)
+                print('new_pos',new_pos,'type',type(new_pos))
+    
+def secplaygame():
+    global turn
+    global board
+    global empty_positions
+    global new_pos
+    if turn ==2:
+        
+        best_position=new_pos
+       
+        print(best_position)
+        
+        board[best_position] = 'X'
+        print(board)
+        if best_position == 0:
+                b0.configure(text='X')
+                b0.configure(state=DISABLED)
+        elif best_position == 1:
+                b1.configure(text='X')
+                b1.configure(state=DISABLED)
+        elif best_position == 2:
+                b2.configure(text='X')
+                b2.configure(state=DISABLED)
+        elif best_position == 3:
+                b3.configure(text='X')
+                b3.configure(state=DISABLED)
+        elif best_position == 4:
+                b4.configure(text='X')
+                b4.configure(state=DISABLED)
+        elif best_position == 5:
+                b5.configure(text='X')
+                b5.configure(state=DISABLED)
+        elif best_position == 6:
+                b6.configure(text='X')
+                b6.configure(state=DISABLED)
+        elif best_position == 7:
+                b7.configure(text='X')
+                b7.configure(state=DISABLED)
+        elif best_position == 8:
+                b8.configure(text='X')
+                b8.configure(state=DISABLED)
+
+        windecide()
+        turn = 1
+        player.config(text="PLAYER ")
+
+def buttonclicked(id):
+    global turn
+    global board
+    
+    if board[id]== "":
+        if turn==1:
+            win.update_idletasks()
+            if id == 0:
+                b0.configure(text='O')
+                b0.configure(state=DISABLED)
+            elif id == 1:
+                b1.configure(text='O')
+                b1.configure(state=DISABLED)
+            elif id == 2:
+                b2.configure(text='O')
+                b2.configure(state=DISABLED)
+            elif id == 3:
+                b3.configure(text='O')
+                b3.configure(state=DISABLED)
+            elif id == 4:
+                b4.configure(text='O')
+                b4.configure(state=DISABLED)
+            elif id == 5:
+                b5.configure(text='O')
+                b5.configure(state=DISABLED)
+            elif id == 6:
+                b6.configure(text='O')
+                b6.configure(state=DISABLED)
+            elif id == 7:
+                b7.configure(text='O')
+                b7.configure(state=DISABLED)
+            elif id == 8:
+                b8.configure(text='O')
+                b8.configure(state=DISABLED)
+            board[id] = 'O'
+            windecide()
+            player.config(text="COMPUTER ")
+            turn=2
+            nextpos()
+            
+        
+
+    
+
+heading = Label(win,text="ONE PLAYER TIC-TAC-TOE",fg="white",bg="blue",height=2,width=20,font=("TimesRoman",12,'bold'))
+player = Label(win,text="PLAYER 1",fg="white",bg="blue",height=2,width=20,font=("TimesRoman",12,'bold'))
+pl = Label(win,text="PLAYER 1 - O",fg="white",bg="blue",height=2,width=10,font=("TimesRoman",12,'bold'))
+p2 = Label(win,text="COMPUTER - X",fg="white",bg="blue",height=2,width=13,font=("TimesRoman",12,'bold'))
+b0=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(0))
+b1=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(1))
+b2=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(2))
+b3=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(3))
+b4=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(4))
+b5=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(5))
+b6=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(6))
+b7=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(7))
+b8=Button(win,text='',fg="black",bg="grey",height=2,width=8,font=("TimesRoman",12,'bold'),command=lambda:buttonclicked(8))
+winner = Label(win,text="",fg="white",bg="blue",height=2,width=20,font=("TimesRoman",12,'bold'))
+
+player.place(x=150,y=300)
+heading.place(x=80,y=10)
+pl.place(x=40,y=50)
+p2.place(x=200,y=50)
+b0.place(x=40,y=100)
+b1.place(x=130,y=100)
+b4.place(x=130,y=150)
+b7.place(x=130,y=200)
+b3.place(x=40,y=150)
+b2.place(x=220,y=100)
+b5.place(x=220,y=150)
+b8.place(x=220,y=200)
+b6.place(x=40,y=200)
+winner.place(x=40,y=250)
+win.mainloop()
